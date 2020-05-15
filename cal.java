7dbe434263e0dc3728821ad7cc6020c947e05f9c
kavindugunathilaka <ukvgunathilaka@students.nsbm.lk>
2020-05-15 13:45:32 +0530

    simple java for testing

---
+++ cal.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class first{
+
+public static void main(String args[]) 	
+
+	{
+	 	Scanner sun = new Scanner(System.in);
+
+		double no1 , no2 , ans ;
+
+		System.out.println("enter first number");
+		no1 = sun.nextDouble();
+
+		System.out.println("enter second number");
+		no2 = sun.nextDouble();
+
+		ans = no1 + no2;
+		System.out.print("anser is : "+ans);
+		
+	 	
+	}
+		   }
+
---
+++ joptionpane.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class first{
+
+public static void main(String args[]){
+
+	String name = JOptionPane.showInputDialog("hii what's ypur name?");
+	JOptionPane.showMessageDialog(null,"i love you"+name);
+
+}
+
+}
